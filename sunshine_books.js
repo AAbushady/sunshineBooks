34cd2cfc8f49ed00094522de54d8ad32ab3ca0b3
Alex Abushady <aabushady214@gmail.com>
2019-08-28 14:10:09 -0400

    Create sunshine_books.js
    Created sunshine_books.js and added comment with problem statement and pseudocode.

---
+++ sunshine_books.js
@@ -0,0 +1,45 @@
+/*
+    Sunshine Books is a bookstore that's open every morning from 8:00 am to
+    12:00 pm. The manager wasts information about the number of customers
+    in the store at different times. A clerk, whose station is near the door,
+    counts customers by making a mark on a piece of paper each time a customer
+    enters. The paper is divided into four segments, one for each hour, and the
+    clerk notes the time before making each mark. When the store closes at noon,
+    the clerk wants to enter the numbers into a program, which then displays
+    the total number of cusomters for the day and the average number of customers
+    per hour.
+    
+    Your job, as the programmer, is to develop a program to calculate the customers
+    for the day, and the average number of customers per hour.
+    
+    Assume the clerk has counted the following customers...
+    
+    Hour:Customers
+    1:75
+    2:96
+    3:22
+    4:108
+    
+    Calculate and Display the total customers for the day and the average customers per hour...
+*/
+
+// Pseudocode
+/*
+    -Variables-
+    Declare Numeric hour1
+    Declare Numeric hour2
+    Declare Numeric hour3
+    Declare Numeric hour4
+    Declare Numeric totalCustomers
+    Declare Constant hours
+    Declare Numeric avgCustomers
+    
+    -Calculate and Display Results-
+    Add hour1, hour2, hour3, and hour4 to totalCustomers
+    Display `The total number of customers is ${totalCustomers}`
+    
+    Divide totalCustomers by hours and assign to avgCustomers
+    Display `The average amount of customers is ${avgCustomers}`
+*/
+
+// Variables
